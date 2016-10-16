292107b95fac7b2c8c21849fdd062ce123f2e372
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-16 18:11:42 +0200

    Removed panel in movies list

--- src/MoviesList.jsx
+++ src/MoviesList.jsx
@@ -12,16 +12,13 @@ class MoviesList extends Component {
   
     render() {
         return (
-            <div className="MoviesList panel panel-primary">
-                <div className="panel-heading">Movies list</div>
-                <div className="panel-body">
-                    <div id="options" className="form-group form-inline">
-                        <SearchFilter onChange={this.props.onFilterChange} id="search-filter" label="Filter:" placeholder="Enter text filter..."/>
-                        <SortList onChange={this.props.onSortChange} id="sort-option" label="Sort:" />
-                    </div>
-                    <div className="list-group">
-                        {this.props.movies.map((m) => (<MovieListItem key={m.id} movie={m} />))}
-                    </div>
+            <div className="MoviesList">
+                <div id="options" className="form-group form-inline">
+                    <SearchFilter onChange={this.props.onFilterChange} id="search-filter" label="Filter:" placeholder="Enter text filter..."/>
+                    <SortList onChange={this.props.onSortChange} id="sort-option" label="Sort:" />
+                </div>
+                <div className="list-group">
+                    {this.props.movies.map((m) => (<MovieListItem key={m.id} movie={m} />))}
                 </div>
             </div>);
     }
