7157cfcdddd912e469efdf8060a904363b30f8bc
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-16 19:00:01 +0200

    Renamed CSS class

--- src/detail/MovieProperty.jsx
+++ src/detail/MovieProperty.jsx
@@ -3,7 +3,7 @@ import './MovieProperty.css';
 
 const MovieProperty = ({label, value}) => (
     <li className="MovieProperty">
-        <span className="property">{label}&nbsp;</span>
+        <span className="label">{label}&nbsp;</span>
         <span className="value">{value}</span>
     </li>
 );
