cf357dc8d591cde0599f1c9c2a0590440a436c87
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-16 12:44:17 +0200

    Simulate remote filtering and sorting

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -10,6 +10,9 @@ const DataProvider = {
           queryString += 'f=' + options.filter;
         }
         if (options.sort) {
+          if (options.filter) {
+            queryString += '&';
+          }
           queryString += 's=' + options.sort;
         }
       }
@@ -18,7 +21,21 @@ const DataProvider = {
         .then(function(response) {
           return response.json()
         }).then(function(json) {
-          callback(json.movies);
+
+          let movies = json.movies.filter((m) => (
+            m.title.indexOf(options.filter) >= 0
+            || m.director.indexOf(options.filter) >= 0
+            || m.year.toString().indexOf(options.filter) >= 0)
+          );
+
+          if (options.sort === 'title') {
+            movies = movies.sort((m1, m2) => (m1.title >= m2.title));
+          }
+          else if (options.sort === 'year') {
+            movies = movies.sort((m1, m2) => (m1.year >= m2.year));
+          }
+
+          callback(movies);
         }).catch(function(ex) {
           console.log('parsing failed', ex)
           callback({});
