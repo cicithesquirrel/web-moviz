e5a561df5de353e88c037eeb6df2531e341fee57
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-16 18:19:50 +0200

    Added movie details

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -5,8 +5,13 @@ class Movie extends Component {
   
     render() {
         return (
-            <div>
+            <div className="Movie">
                 <h2>{this.props.movie.title}</h2>
+                <ul>
+                    <li><span className="property">Director(s):</span> <span className="value">{this.props.movie.director}</span></li>
+                    <li><span className="property">Year:</span> <span className="value">{this.props.movie.year}</span></li>
+                    <li><span className="property">Duration (min):</span> <span className="value">{this.props.movie.duration}</span></li>
+                </ul>
             </div>);
     }
 }
