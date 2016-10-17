0aa4462ade512b12a88e6cff465d1a2d80d7c404
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-17 19:30:56 +0200

    Refactor using lambda expr

--- src/list/MoviesList.jsx
+++ src/list/MoviesList.jsx
@@ -1,27 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import MovieListItem from './MovieListItem';
 import './MoviesList.css';
 import SearchFilter from './SearchFilter';
 import SortList from './SortList';
 
-class MoviesList extends Component {
-    constructor(props) {
-        super();
-        this.props = props;
-    }
-  
-    render() {
-        return (
-            <div className="MoviesList">
-                <div id="options" className="form-group form-inline">
-                    <SearchFilter onChange={this.props.onFilterChange} id="search-filter" label="Filter:" placeholder="Enter text filter..."/>
-                    <SortList onChange={this.props.onSortChange} id="sort-option" label="Sort:" />
-                </div>
-                <div className="list-group">
-                    {this.props.movies.map((m) => (<MovieListItem key={m.id} movie={m} />))}
-                </div>
-            </div>);
-    }
-}
+const MoviesList = ({onFilterChange, onSortChange, movies}) => (
+    <div className="MoviesList">
+        <div id="options" className="form-group form-inline">
+            <SearchFilter onChange={onFilterChange} id="search-filter" label="Filter:" placeholder="Enter text filter..."/>
+            <SortList onChange={onSortChange} id="sort-option" label="Sort:" />
+        </div>
+        <div className="list-group">
+            {movies.map((m) => (<MovieListItem key={m.id} movie={m} />))}
+        </div>
+    </div>
+);
 
 export default MoviesList;
