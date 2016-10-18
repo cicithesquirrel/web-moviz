2a50fc7cd597011aaff61e5ca2bcfcb9e63d4978
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-18 21:02:32 +0200

    Addd button to clear filter

--- src/list/SearchFilter.jsx
+++ src/list/SearchFilter.jsx
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
+import "./SearchFilter.css";
 
 class SearchFilter extends Component {
     constructor(props) {
         super(props);
         // We need binding to be able to use 'this' in the callback
-        this.onChange = this._onChange.bind(this)
+        this.onChange = this._onChange.bind(this);
+        this.onClear = this._onClear.bind(this);
     }
 
     _onChange(e) {
         // Forward the change event after extracting the filter value
         this.props.onChange(e.target.value);
     }
+
+    _onClear(e) {
+        // TODO clear filter
+    }
   
     render() {
         return (
             <div className="form-group">
                 <label htmlFor={this.props.id}>{(this.props.label?this.props.label:'Filter:')}</label>
-                <input onChange={this.onChange} type="text" className="form-control" id={this.props.id} placeholder={this.props.placeholder} value={this.props.value} />
+                <div className="input-group">
+                    <input onChange={this.onChange} type="text" className="form-control" id={this.props.id} placeholder={this.props.placeholder} value={this.props.value} />
+                    <span className="input-group-btn">
+                        <button onClick={this.onClear} id="SearchFilterClearButton" className="btn btn-default" type="button">
+                            <i className="fa fa-times"></i>
+                        </button>
+                    </span>
+                </div>
             </div>
         );
     }
