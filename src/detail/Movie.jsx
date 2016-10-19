52e7516c4ac6ce7a5b0ddf2cd357296950f7a343
cchene <cchene@e-citiz.com>
2016-10-19 15:17:28 +0200

    Added link to IMDB

--- src/detail/Movie.jsx
+++ src/detail/Movie.jsx
@@ -10,7 +10,9 @@ class Movie extends Component {
         let countryImage = <img src={'/flags/' + movie.country + '.png'} alt={movie.country} title={this.props.movie.country}/>;
         return (
             <div className="Movie">
-                <h2>{movie.title}</h2>
+                <h2>{movie.title}&nbsp;
+                    {movie['imdb-ref'] && <a target="_blank" href={'http://www.imdb.com/title/' + movie['imdb-ref']} title="Go to IMDB"><i className="fa fa-globe"></i></a>}
+                </h2>
                 {movie['alt-titles'] && <MovieAltTitles titles={movie['alt-titles']} />}
                 <section>
                     <h3>Details</h3>
