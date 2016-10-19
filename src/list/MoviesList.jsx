2a3d611cf17d4d35ec81c7fc0e9176fe464d2719
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-10-19 08:11:36 +0200

    Changed "See stats" link position

--- src/list/MoviesList.jsx
+++ src/list/MoviesList.jsx
@@ -9,11 +9,11 @@ const MoviesList = ({onFilterChange, onSortChange, movies, sort, filter}) => (
         <div id="options" className="form-group form-inline">
             <SearchFilter onChange={onFilterChange} id="search-filter" label="Filter:" placeholder="Enter text filter..." value={filter}/>
             <SortList onChange={onSortChange} id="sort-option" label="Sort:"  value={sort}/>
+            <a href="/stats"><i className="fa fa-area-chart"></i> See stats</a>
         </div>
         <div className="list-group">
             {movies.map((m) => (<MovieListItem key={m.id} movie={m} />))}
         </div>
-        <a href="/stats">See stats</a>
     </div>
 );
 
